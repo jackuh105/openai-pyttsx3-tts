64589ab23e1156d951c996328db883eda70d9e00
Jack Cheang <uhcheang@safp.gov.mo>
2025-11-28 10:29:30 +0800

    change default port

--- server.py
+++ server.py
@@ -98,4 +98,4 @@ async def list_voices():
     return {"voices": voices}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=4000)
+    uvicorn.run(app, host="0.0.0.0", port=8100)
